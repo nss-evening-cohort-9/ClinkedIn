2a1d62d92317f28e8483e0e71f1e5712a6fbf6dd
Keith Walker <Keithrwalker418@gmail.com>
2019-09-30 21:06:31 -0500

    added seed clinkers, and get all/id

--- ClinkedIn/Models/Clinker.cs
+++ ClinkedIn/Models/Clinker.cs
@@ -7,5 +7,13 @@ namespace ClinkedIn.Models
 {
     public class Clinker
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int InmateNum { get; set; }
+        public List<Guid> FriendsList { get; set; }
+        public List<Guid> EnemiesList { get; set; }
+        public List<Guid> Services { get; set; }
+        public List<Guid> Interests { get; set; }
+
     }
 }
